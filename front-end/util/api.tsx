83c08613169f5b2fb313428d0e56790f9c8c1996
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2025-03-26 02:12:38 -0700

    Fix: fixed Type declaration errors Feat: cont. allowed starting the server and calling APIs on configurable ports

--- front-end/util/api.tsx
+++ front-end/util/api.tsx
@@ -1,28 +1,23 @@
 import axios from 'axios';
-
-type TransactionRequest = {
-    sender: string;
-    receiver: string;
-    amount: number;
-};
+import {Block, Transaction, TransactionRequest} from "./types"
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://localhost:" + __API_PORT__,
     timeout: 1000
 });
 
 /**
  * send a new Transaction POST request to the API
  * @param txn the transaction request 
- * @returns {Boolean} transaction sending status: 
+ * @returns {boolean} transaction sending status: 
  *      if successfully sending a transaction, return true,
  *      if the sending failed, return false
  */
-export const sendTransaction = async (txn: TransactionRequest): Promise<any> => {
+export const sendTransaction = async (txn: TransactionRequest): Promise<boolean> => {
     try {
         const response = await api.post("/send-transaction", txn);
         console.log("[sendTransaction] result", response);
-        return response;
+        return response.data;
     } catch (error) {
         console.error("[sendTransaction] error", error);
         throw error;
@@ -32,13 +27,13 @@ export const sendTransaction = async (txn: TransactionRequest): Promise<any> => 
 /**
  * send a mine-block POST request to the backend API
  * @param None
- * @returns {Object} a new Block in the format of JSON
+ * @returns {Block} a new Block in the format of JSON
  */
-export const mineBlock = async () => {
+export const mineBlock = async (): Promise<Block> => {
     try {
         const newBlock = await api.post("/mine-block");
         console.log("[mineBlock] result", newBlock);
-        return newBlock;
+        return newBlock.data;
     } catch (error) {
         console.error("[mineBlock] error", error);
         throw error;
@@ -48,13 +43,13 @@ export const mineBlock = async () => {
 /**
  * send a balance GET request to the backend API
  * @param None
- * @returns {Number} an integer if the request is handled successfully
+ * @returns {number} an integer if the request is handled successfully
  */
-export const fetchUserBalance = async () => {
+export const fetchUserBalance = async (): Promise<number> => {
     try {
         const balance = await api.get("/user-balance");
         console.log("[fetchUserBalance] result", balance);
-        return balance;
+        return balance.data;
     } catch (error) {
         console.error("[fetchUserBalance] error", error);
         throw error;
@@ -64,14 +59,14 @@ export const fetchUserBalance = async () => {
 /**
  * send a blockchain GET request to the backend API
  * @param None
- * @returns {Object} a JSON that
- *      shows blocks that the p2p node has confirmed
+ * @returns {Array<Block>} a JSON that
+ *      shows an array blocks that the p2p node has confirmed
  */
-export const fetchBlockchain = async () => {
+export const fetchBlockchain = async (): Promise<Array<Block>> => {
     try {
         const blockchain = await api.get("/blockchain");
         console.log("[fetchBlockchain] result", blockchain);
-        return blockchain;
+        return blockchain.data;
     } catch (error) {
         console.error("[fetchBlockchain] error", error);
         throw error;
@@ -81,14 +76,14 @@ export const fetchBlockchain = async () => {
 /**
  * send a transaction pool GET request to the backend API
  * @param None
- * @returns {Object} a JSON that
+ * @returns {Array<Transaction>} a JSON that
  *      shows the transactions that have not been mined
  */
-export const fetchTransactionPool = async () => {
+export const fetchTransactionPool = async (): Promise<Array<Transaction>> => {
     try {
         const transactionPool = await api.get("/transaction-pool");
         console.log("[fetchTransactionPool] result", transactionPool);
-        return transactionPool;
+        return transactionPool.data;
     } catch (error) {
         console.error("[fetchTransactionPool] error", error);
         throw error;
@@ -98,13 +93,13 @@ export const fetchTransactionPool = async () => {
 /**
  * send a log GET request to the backend API
  * @param None
- * @returns {Array} the logs of the backend, including P2P network behaviors and blockchain behaviors
+ * @returns {Array<string>} the logs of the backend, including P2P network behaviors and blockchain behaviors
  */
-export const fetchLogs = async () => {
+export const fetchLogs = async (): Promise<Array<string>> => {
     try {
         const response = await api.get("/logs");
         console.log("[fetchLogs] result", response);
-        return response;
+        return response.data;
     } catch (error) {
         console.error("[fetchLogs] error", error);
         throw error;
